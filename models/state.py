9a989ac67daf39a81f3049fcc7ae5455f646f894
Amine Lamuadni <W7equwE5@gmail.com>
2023-11-12 23:35:27 +0000

    Add state.py file

---
+++ models/state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+state.py
+Module that defines the State class.
+"""
+
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    State class that inherits from BaseModel.
+    """
+    name = ""
