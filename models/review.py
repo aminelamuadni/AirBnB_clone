03008ac95a02de38d1729e0e7ec344001534ea4a
Amine Lamuadni <W7equwE5@gmail.com>
2023-11-12 23:44:06 +0000

    Update review.py file

--- models/review.py
+++ models/review.py
@@ -11,6 +11,11 @@ from models.base_model import BaseModel
 class Review(BaseModel):
     """
     Review class that inherits from BaseModel.
+
+    Attributes:
+        place_id (str): The Place.id of the place the review is for.
+        user_id (str): The User.id of the user who wrote the review.
+        text (str): The text of the review.
     """
     place_id = ""
     user_id = ""
