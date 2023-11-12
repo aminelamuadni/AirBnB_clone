01a1e00993e002cf90962623e8b1da87c0be2fd0
Amine Lamuadni <W7equwE5@gmail.com>
2023-11-12 23:35:27 +0000

    Add amenity.py file

---
+++ models/amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+amenity.py
+Module that defines the Amenity class.
+"""
+
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class that inherits from BaseModel.
+    """
+    name = ""
