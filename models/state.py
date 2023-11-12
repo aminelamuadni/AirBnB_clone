821d58b7975f88077decd5ea5d746c09a9515132
Amine Lamuadni <W7equwE5@gmail.com>
2023-11-12 23:44:06 +0000

    Update state.py file

--- models/state.py
+++ models/state.py
@@ -11,5 +11,8 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """
     State class that inherits from BaseModel.
+
+    Attributes:
+        name (str): The name of the state.
     """
     name = ""
