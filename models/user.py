d106fae908223e4a5da7889c1d95ea23692c4066
Amine Lamuadni <W7equwE5@gmail.com>
2023-11-12 23:48:04 +0000

    Update user.py file

--- models/user.py
+++ models/user.py
@@ -12,7 +12,15 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represents a User in the AirBnB clone project."""
+    """
+    Represents a User in the AirBnB clone project.
+
+    Attributes:
+        email (str): Email address of the user.
+        password (str): Password for the user account.
+        first_name (str): First name of the user.
+        last_name (str): Last name of the user.
+    """
     email = ""
     password = ""
     first_name = ""
