d76f2ed17f54df28a86f76af84c482e86044b987
Amine Lamuadni <W7equwE5@gmail.com>
2023-11-15 01:48:41 +0000

    Update base_model.py file

--- models/base_model.py
+++ models/base_model.py
@@ -67,7 +67,7 @@ class BaseModel:
         Updates the public instance attribute updated_at with the current
         datetime.
         """
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
